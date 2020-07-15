e4aa8c30733607e06e268be35009aa7344ca1105
Jeremy Huot <logflume@live.com>
2020-07-15 20:10:07 +0000

    Updated client.js to handle axaj requests to post from the new tweet form to the server, and get tweets from the server to load on the page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,30 +9,30 @@
 
 
 // Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 
 
@@ -69,7 +69,26 @@ $(document).ready(function(){
       $('section.tweets').append(createTweetElement(tweet));
     }
   };
-  renderTweets(data);
+  // renderTweets(data);
+
+  $('#new-tweet-form').submit(function(event) {
+    event.preventDefault();
+    $.ajax({ url: '/tweets/', data: $(this).serialize() })
+    .then((res) => {
+      console.log(res);
+    })
+    .fail((err) => {
+    });
+  });
+  
+  const loadTweets = function() {
+    $.ajax('/tweets/', { method: 'GET' })
+    .then((res) => {
+      renderTweets(res);
+    })
+  };
+  loadTweets();
+
 });
 
 
